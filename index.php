5aad927b6f5e5d370fa7599c4c89896188281880
Vladyslav Perepelytsia <perepelytsia.vladyslav@gmail.com>
2017-03-28 19:29:04 +0300

    23032017c

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(E_ALL); // будет выводить ошибки
 
 define('DS', DIRECTORY_SEPARATOR); // вводим константу, содержащую разделитель пути, для Windows это «\»
 // Узнаём путь к файлам сайта
